6d623a0fe015538c778fcce5a6a4c424b138a9b7
ross-mclain-br <106842863+ross-mclain-br@users.noreply.github.com>
2023-07-19 23:22:41 -0400

    started up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2f056b",
+        secondary: "#f9e1cf",
+        tertiary: "#44079c",
+        quaternary: "#1a033a",
+        quinary: "#fffdfc",
+        sexinary: "#f9e1cf",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
