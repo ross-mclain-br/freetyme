f9a9bf1acdaf727940fe0fedb9545c2dd6ce0d6b
ross-mclain-br <106842863+ross-mclain-br@users.noreply.github.com>
2023-07-21 00:14:38 -0400

    Lets get going

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,20 @@ import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Layout from "~/components/layout";
 
+import { JetBrains_Mono } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"] });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ClerkProvider>
+    <div className={jetbrainsMono.className}>
+      <ClerkProvider {...pageProps}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ClerkProvider>
+    </div>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,16 @@ export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
-    <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-      <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-        Freetyme
-      </h1>
+    <div className=" flex flex-col items-center gap-12 px-4 py-24 ">
+      
+      <input
+        type="text"
+        name="search"
+        id="search"
+        className="block  rounded-md border-0 bg-transparent py-3 font-extrabold tracking-tight text-center opacity-20 text-secondary  placeholder:text-secondary focus:ring-0 sm:text-6xl "
+        placeholder="Find Your Freetyme!"
+      />
       <div className="flex flex-col items-center justify-center gap-4"></div>
-      {/* <p className="text-2xl text-white">
-        {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-      </p> */}
     </div>
   );
 }
